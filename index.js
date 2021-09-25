48004116ce03e30eb1ef720a52772c81595a2154
Leon <hualiang.tian@gmail.com>
2021-09-24 20:19:15 -0400

    commit tree 20ce19c6bb9cc402ef708d40460414b3eeb17103

--- index.js
+++ index.js
@@ -1,1 +1,74 @@
 // Your code here
+function mapToNegativize(sourceArray)
+{
+    let newA =[];
+    for (let i=0; i< sourceArray.length; i++)
+    {
+        newA.push(sourceArray[i]*-1);
+    }
+    return newA;
+}
+
+function mapToNoChange(sourceArray)
+{
+    let newB = [];
+    for (let i=0; i< sourceArray.length; i++)
+    {
+        newB.push(sourceArray[i]);
+    }
+    return newB;
+}
+
+function mapToDouble(sourceArray)
+{
+    let newC = [];
+    for (let i = 0; i < sourceArray.length; i++)
+    {
+        newC.push(sourceArray[i]*2);
+    }
+    return newC
+}
+
+function mapToSquare(sourceArray)
+{
+    let newD = [];
+    for (let i = 0; i < sourceArray.length; i++)
+    {
+        newD.push(sourceArray[i]**2);
+    }
+    return newD;
+}
+
+
+
+function reduceToTotal(sourceArray, startingPoint=0)
+{
+    let total = startingPoint
+    for (let i = 0; i < sourceArray.length; i++)
+    {
+        total = sourceArray[i] + total
+    }
+    return total;
+}
+
+
+function reduceToAllTrue(sourceArray)
+{
+    for (let i = 0; i < sourceArray.length; i++)
+    {
+        if (!sourceArray[i])
+            return false
+    }
+    return true;
+}
+
+
+function reduceToAnyTrue(sourceArray)
+{
+    for (let i = 0; i < sourceArray.length; i++)
+    {
+        if (sourceArray[i])
+            return true
+    }
+    return false;
+}
